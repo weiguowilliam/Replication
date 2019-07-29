b6f9bbbb4fe1f9a1e5d7bf489110f726a1d1c2de
Wei Guo <weiguo@SEAS14710.local>
2019-07-29 14:50:14 -0400

    MaxDepthASTNode

--- de-anonymizing/test/MaxDepthASTNode.py
+++ de-anonymizing/test/MaxDepthASTNode.py
@@ -7,32 +7,22 @@ from clang.cindex import CursorKind
 
 Config.set_library_file("/usr/local/Cellar/llvm/8.0.0_1/lib/libclang.dylib")
 
-def get_ast(cursor, level = 0):
-    print level
+
+
+def get_ast(cursor, level = 0, depthlist = []):
+    """
+    store the depth of each node into depthlist, use max(depthlist) to get the MaxDepthASTNode of each tree
+    """
+    depthlist.append(level)
     for c in cursor.get_children():
-        get_ast(c,  level+1)
-        
-
-class features(object):
-    def __init__(self):
-        self.depth = []
-
-    def get_ast(self, cursor, level = 0):
-        self.depth.append(level)
-        print level
-        for c in cursor.get_children():
-            get_ast(c,  level+1)
-        
+        get_ast(c,  level+1, depthlist)
 
 if __name__ == '__main__':
     index = clang.cindex.Index.create()
     tu = index.parse('test1.cpp')
     print 'Translation unit:', tu.spelling
-    a = features()
-    a.get_ast(tu.cursor)
-    print a.depth
-    # get_ast(tu.cursor)
+    dll = [0]
+    get_ast(cursor = tu.cursor, level = 0, depthlist= dll)
+    print max(dll)
 
-
-#TODO
-#store the depth to a list and return max(depth list)
+        
